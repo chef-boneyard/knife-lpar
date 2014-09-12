eef1a387d8a97afda4126e6b35416dc928e11710
Scott Hain <shain@getchef.com>
2014-09-12 15:30:46 -0700

    Base tests complete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,8 +22,4 @@ RSpec.configure do |c|
   end
   c.filter_run :focus => true
   c.run_all_when_everything_filtered = true
-<<<<<<< HEAD
-=======
-  c.treat_symbols_as_metadata_keys_with_true_values = true
->>>>>>> Some failing tests
 end
