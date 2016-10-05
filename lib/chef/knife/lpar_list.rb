bc76ebe1cca99c3b9e024699f02c9f084270bae4
Tim Smith <tsmith@chef.io>
2016-10-04 21:09:01 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/knife/lpar_list.rb
+++ lib/chef/knife/lpar_list.rb
@@ -1,6 +1,6 @@
 #
-# Author:: Julian C. Dunn (<jdunn@getchef.com>)
-# Copyright:: Copyright (c) 2014 Chef Software Inc.
+# Author:: Julian C. Dunn (<jdunn@chef.io>)
+# Copyright:: Copyright (c) 2014-2016 Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
